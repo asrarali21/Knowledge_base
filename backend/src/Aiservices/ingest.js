81aaa1da368956af2a2450d1d8566d4b64b99d7e
asrarali21 <mohdasrar3825@gmail.com>
2025-10-13 15:33:54 +0530

    ingestion done! kb created

--- backend/src/Aiservices/ingest.js
+++ backend/src/Aiservices/ingest.js
@@ -1,16 +1,52 @@
 
+import dotenv from "dotenv"
 import {PDFLoader} from "@langchain/community/document_loaders/fs/pdf"
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { QdrantVectorStore } from "@langchain/qdrant";
 
-
+dotenv.config({ path: '../../.env' })
     const file_path = "/Users/mohammedasrarali/Desktop/data/R22B.Tech.CSECourseStructureSyllabus2.pdf"
     const loader  =  new PDFLoader(file_path)
 
 const ingestDocs =async ()=>{
+
+    console.log("api key of gemi",process.env.GEMINI_API_KEY);
+    
     
 
     const docs =await  loader.load()
 
-    console.log(docs[0])
+
+
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize:1000,
+        chunkOverlap:200,
+    })
+
+
+    const splitDocument = await splitter.splitDocuments(docs)
+
+
+    const embedding = new GoogleGenerativeAIEmbeddings({
+        apiKey:process.env.GEMINI_API_KEY,
+       model: process.env.GEMINI_EMBED_MODEL || "text-embedding-004",
+    })
+
+
+  
+   const vectorStore = await QdrantVectorStore.fromDocuments(
+    docs,
+    embedding,{
+        url:"http://localhost:6333",
+        collectionName:"regulations"
+    }
+   )
+    
+
+
+    console.log("Ingestion complete! The knowledge base has been created.");
+     
   
 }
-ingestDocs()
+ingestDocs().catch(console.error)
